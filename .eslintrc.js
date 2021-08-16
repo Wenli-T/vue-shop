6d08f92645b8515ec2d33b2b21f167557038cdb4
Wenli-T <tianwenli913@163.com>
2021-08-16 20:41:34 +0800

    完成商品分类功能

--- .eslintrc.js
+++ .eslintrc.js
@@ -15,6 +15,7 @@ module.exports = {
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'comma-dangle': 0,
     'space-before-function-paren': 0,
-    'vue/no-unused-vars': 0
+    'vue/no-unused-vars': 0,
+    'no-useless-return': 0
   }
 }
--- src/main.js
+++ src/main.js
@@ -6,6 +6,8 @@ import './plugins/element.js'
 import './assets/fonts/iconfont.css'
 // 导入全局样式表
 import './assets/css/global.css'
+// 导入第三方组件
+import TreeTable from 'vue-table-with-tree-grid'
 
 import axios from 'axios'
 // 配置请求的根路径
@@ -22,6 +24,9 @@ axios.interceptors.request.use(config => {
 Vue.prototype.$http = axios
 
 Vue.config.productionTip = false
+
+// 全局注册第三方组件
+Vue.component('tree-table', TreeTable)
 
 new Vue({
   router,
